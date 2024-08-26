cbb8afb1107e8a9177f932f2d6a440997794e0d0
Ronald Pondo <rrpondo@gmail.com>
2024-08-26 17:24:14 +0200

    Fix deployment

--- app/api/auth/success/route.ts
+++ app/api/auth/success/route.ts
@@ -1,6 +1,6 @@
-import prisma from "@/lib/prisma";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
 
 export async function GET() {
    const {getUser} = await getKindeServerSession();
